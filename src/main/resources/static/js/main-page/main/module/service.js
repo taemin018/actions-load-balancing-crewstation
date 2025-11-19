5de2e23a796ce038f06c50de19a6d0338a3d1f2a
김태민 <gtaemin254@gmail.com>
2025-11-19 11:46:06 +0900

    init

--- src/main/resources/static/js/main-page/main/module/service.js
+++ src/main/resources/static/js/main-page/main/module/service.js
@@ -36,7 +36,7 @@ const recommendService = (() => {
     };
 
     const sendMyInfoDatas = async (myInfoDatas) => {
-        const response = await fetch(`http://127.0.0.1:8000/api/userdata`, {
+        const response = await fetch(`/api/userdata`, {
             method: "POST",
             headers: {
                 "Content-Type": "application/json",
