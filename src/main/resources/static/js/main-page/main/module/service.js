602f8b50b885ba11614dfc61b303477146cbf74e
김태민 <gtaemin254@gmail.com>
2025-11-19 11:09:49 +0900

    init

--- src/main/resources/static/js/main-page/main/module/service.js
+++ src/main/resources/static/js/main-page/main/module/service.js
@@ -35,7 +35,24 @@ const recommendService = (() => {
         return data;
     };
 
-    return { getUserRecommendData : getUserRecommendData};
+    const sendMyInfoDatas = async (myInfoDatas) => {
+        const response = await fetch("http://localhost:8000/api/userdata", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({myInfoDatas: myInfoDatas})
+        });
+
+        if (!response.ok) {
+            throw new Error("요청 실패");
+        }
+
+        const data = await response.json();
+        return data;
+    };
+
+    return { getUserRecommendData : getUserRecommendData, sendMyInfoDatas: sendMyInfoDatas};
 })();
 
 
